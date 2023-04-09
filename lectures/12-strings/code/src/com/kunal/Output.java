b2c8b09a5e4a949f1a62c252905db80e613045f5
Ayush Goel <116777368+ayushg4@users.noreply.github.com>
2023-04-09 21:25:42 +0530

    Update Output.java

--- lectures/12-strings/code/src/com/kunal/Output.java
+++ lectures/12-strings/code/src/com/kunal/Output.java
@@ -5,13 +5,20 @@ import java.util.Arrays;
 public class Output {
     public static void main(String[] args) {
         System.out.println(56);
-//        Integer num = new Integer(56);
-//        System.out.println(num.toString());
-//        System.out.println(num);
-//        System.out.println("Kunal");
-//        System.out.println(Arrays.toString(new int[]{2, 3, 4, 5}));
-////
-//        String name = null;
-//        System.out.println(name);
+        System.out.println("Kunal");
+        
+        System.out.println(new int[]{2, 3, 4, 5});
+        // prints a random code like I@43553
+        System.out.println(Arrays.toString(new int[]{2, 3, 4, 5}));
+        // prints a proper array
+        // here we are overriding the default java method by describing our method
+        
+         String name = null;
+     System.out.println(name);          // prints null
+        
+    Integer num = new Integer(56);
+        // creating wrapper class
+    System.out.println(num.toString());  // diff such functions can be used now on integer
+      System.out.println(num);       // prints "56"
     }
 }

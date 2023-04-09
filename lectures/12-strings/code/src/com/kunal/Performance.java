9e84ea987c257af2daab4b1af056469a250a1afd
Ayush Goel <116777368+ayushg4@users.noreply.github.com>
2023-04-09 22:25:16 +0530

    Update Performance.java

--- lectures/12-strings/code/src/com/kunal/Performance.java
+++ lectures/12-strings/code/src/com/kunal/Performance.java
@@ -5,9 +5,15 @@ public class Performance {
         String series = "";
         for (int i = 0; i < 26; i++) {
             char ch = (char)('a' + i);
-            series = series + ch; // series += ch
+            series = series + ch; // series += ch 
+               
         }
 
         System.out.println(series);
+          // prints abcdefghij...z
+        // not a good method bcz in each iteration a new string is being created
+        // while the pevious one is left behind wasting memory
+        
+        // so therefore we use StringBuilder
     }
 }

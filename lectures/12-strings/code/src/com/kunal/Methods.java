32553fe89f74c53b6b8175ffd5d1715e30edf32d
Ayush Goel <116777368+ayushg4@users.noreply.github.com>
2023-04-09 22:34:29 +0530

    Update Methods.java

--- lectures/12-strings/code/src/com/kunal/Methods.java
+++ lectures/12-strings/code/src/com/kunal/Methods.java
@@ -6,8 +6,8 @@ public class Methods {
     public static void main(String[] args) {
         String name = "Kunal Kushwaha Hello World";
         System.out.println(Arrays.toString(name.toCharArray()));
-        System.out.println(name.toLowerCase());
-        System.out.println(name);
+        System.out.println(name.toLowerCase());  // creates new object(string) and prints it
+        System.out.println(name);  // this remains same as original
         System.out.println(name.indexOf('a'));
         System.out.println("     Kunal   ".strip());
         System.out.println(Arrays.toString(name.split(" ")));

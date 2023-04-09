de0ee1aa97749edbf71179cb0ca44eaf9586cc75
Ayush Goel <116777368+ayushg4@users.noreply.github.com>
2023-04-09 21:14:24 +0530

    Update Comparison.java

--- lectures/12-strings/code/src/com/kunal/Comparison.java
+++ lectures/12-strings/code/src/com/kunal/Comparison.java
@@ -5,16 +5,23 @@ public class Comparison {
         String a = "Kunal";
         String b = "Kunal";
         String c = a;
-//        System.out.println(c == a);
-        // ==
-//        System.out.println(a == b);
+      System.out.println(c == a);  // returns True
+        
+        // == compares both the values and also whether their ref variables point to same 
+        // .equals() checks value only
+        
+    System.out.println(a == b);  // returns True
 
-        String name1 = new String("Kunal");
-        String name2 = new String("Kunal");
+        String name1 = new String("Kunal");  // this way Kunal is created outside the string pool
+                                             // in heap memory
+        String name2 = new String("Kunal");   // both are at different addresses
 
-        System.out.println(name1 == name2);
+        System.out.println(name1 == name2);  // returns False
 
-        System.out.println(name1.equals(name2));
+        System.out.println(name1.equals(name2)); //  returns True
+        
+        System.out.println(name1.charAt(0));
+        // to get a single character from a string
 
     }
 }

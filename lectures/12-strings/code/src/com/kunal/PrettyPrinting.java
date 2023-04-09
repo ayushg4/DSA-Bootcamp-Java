9fb2cbc6b98321d5a0ba9e3fbb85dcec4d3123a1
Ayush Goel <116777368+ayushg4@users.noreply.github.com>
2023-04-09 22:05:15 +0530

    Update PrettyPrinting.java

--- lectures/12-strings/code/src/com/kunal/PrettyPrinting.java
+++ lectures/12-strings/code/src/com/kunal/PrettyPrinting.java
@@ -8,5 +8,18 @@ public class PrettyPrinting {
 //        System.out.printf("Pie: %.3f", Math.PI);
 
         System.out.printf("Hello my name is %s and I am %s", "Kunal", "Cool");
+        
+ // %c-Character
+// %d - Decimal number (base 10)
+// %e- Exponential floating-point number
+// %f - Floating-point number
+// %i- Integer (base 10)
+// %0-Octal number (base 8)
+// %s - String
+// %u - Unsigned decimal (integer) number
+//  %x - Hexadecimal number (base 16)
+//  %t- Date/time
+// %n - Newline
+
     }
 }
